0a8722c5960fc160ad4bbbac617540086236517e
ShemetovDanila <0illiuwuilli0@gmail.com>
2026-05-28 02:44:42 +0700

    lab10

---
+++ lab10/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(352, 12);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(584, 714);
+            chart1.TabIndex = 0;
+            chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(89, 529);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 112);
+            button1.TabIndex = 1;
+            button1.Text = "Старт";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(325, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Интенсивность поступления заявок (лямбда)";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(325, 27);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "0.5";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(21, 162);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(325, 27);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "2";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(282, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Интенсивность обработки заявок (мю)";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(21, 251);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(325, 27);
+            textBox3.TabIndex = 7;
+            textBox3.Text = "10000";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 219);
+            label3.Name = "label3";
+            label3.Size = new Size(141, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Количество заявок";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(976, 12);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Результат";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(21, 346);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(325, 27);
+            textBox4.TabIndex = 10;
+            textBox4.Text = "10";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(21, 314);
+            label4.Name = "label4";
+            label4.Size = new Size(279, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Количество приборов (обработчиков)";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(21, 439);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(325, 27);
+            textBox5.TabIndex = 12;
+            textBox5.Text = "15";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(21, 407);
+            label5.Name = "label5";
+            label5.Size = new Size(118, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Мест в очереди";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1450, 738);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(chart1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+    }
+}
---
+++ lab10/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,252 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label6.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text.Replace(',', '.'),
+                   System.Globalization.NumberStyles.Any,
+                   System.Globalization.CultureInfo.InvariantCulture,
+                   out double lambda) || lambda <= 0)
+            {
+                MessageBox.Show("Введите корректное значение λ.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!double.TryParse(textBox2.Text.Replace(',', '.'),
+                   System.Globalization.NumberStyles.Any,
+                   System.Globalization.CultureInfo.InvariantCulture,
+                   out double mu) || mu <= 0)
+            {
+                MessageBox.Show("Введите корректное значение μ.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!int.TryParse(textBox3.Text, out int totalRequests) || totalRequests <= 0)
+            {
+                MessageBox.Show("Введите корректное количество заявок.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(textBox4.Text, out int N) || N <= 0)
+            {
+                MessageBox.Show("Введите корректное количество приборов (N).", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(textBox5.Text, out int M) || M < 0)
+            {
+                MessageBox.Show("Введите корректный размер очереди (M).", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var rng = new MultRandom(DateTime.Now.Ticks & 0x7FFFFFFF);
+
+            double GenExp(double rate)
+            {
+                double u = rng.Next();
+                u = Math.Max(u, 1e-10);
+                return -Math.Log(1 - u) / rate;
+            }
+
+            double t = 0.0;
+            int x = 0;
+            int y = 0;
+
+            int processedEvents = 0;
+            int accepted = 0;
+            int rejected = 0;
+            int maxState = N + M;
+
+            double[] stateDurations = new double[N + M + 1];
+            double totalQueueTime = 0.0;
+            double totalBusyTime = 0.0;
+
+            double tau = GenExp(lambda);
+            double delta = double.PositiveInfinity;
+
+            while (processedEvents < totalRequests)
+            {
+                double timeStep = 0.0;
+
+                if (tau < delta)
+                {
+                    timeStep = tau;
+                    int currentState = x + y;
+                    if (currentState < stateDurations.Length)
+                    {
+                        stateDurations[currentState] += timeStep;
+                    }
+                    totalQueueTime += y * timeStep;
+                    totalBusyTime += x * timeStep;
+
+                    if (x < N)
+                    {
+                        x = x + 1;
+                        accepted++;
+                    }
+                    else if (y < M)
+                    {
+                        y = y + 1;
+                        accepted++;
+                    }
+                    else
+                    {
+                        rejected++;
+                    }
+
+                    t = t + tau;
+                    delta = delta - tau;
+                    tau = GenExp(lambda);
+                }
+                else
+                {
+                    timeStep = delta;
+                    int currentState = x + y;
+                    if (currentState < stateDurations.Length)
+                    {
+                        stateDurations[currentState] += timeStep;
+                    }
+                    totalQueueTime += y * timeStep;
+                    totalBusyTime += x * timeStep;
+
+                    if (y == 0)
+                    {
+                        x = x - 1;
+                    }
+                    else
+                    {
+                        y = y - 1;
+                    }
+
+                    t = t + delta;
+                    tau = tau - delta;
+                    delta = double.PositiveInfinity;
+                }
+
+                if (x > 0)
+                {
+                    delta = GenExp(mu * x);
+                }
+                else
+                {
+                    delta = double.PositiveInfinity;
+                }
+
+                processedEvents++;
+            }
+
+            double rho = lambda / mu;
+            double avgBusyServers = t > 0 ? totalBusyTime / t : 0.0;
+            double avgQueueLength = t > 0 ? totalQueueTime / t : 0.0;
+            double p1Emp = avgBusyServers / N;
+            double p_rej_stat = (double)rejected / totalRequests;
+
+            double[] probabilities = new double[maxState + 1];
+            for (int i = 0; i <= maxState; i++)
+            {
+                probabilities[i] = t > 0 ? stateDurations[i] / t : 0.0;
+            }
+
+            label6.Text =
+                "═══════════════════════════════════════\r\n" +
+                "          РЕЗУЛЬТАТЫ МОДЕЛИРОВАНИЯ     \r\n" +
+                "═══════════════════════════════════════\r\n" +
+                $"  λ = {lambda:F4}   μ = {mu:F4}   N = {totalRequests}\r\n" +
+                $"  Приборов (N): {N}   Мест в очереди (M): {M}\r\n" +
+                $"  Принято: {accepted}   Отклонено: {rejected}\r\n" +
+                $"  Модельное время: {t:F4}\r\n" +
+                "───────────────────────────────────────\r\n" +
+                $"  1. Коэффициент загрузки: ρ = {rho:F4}\r\n\r\n" +
+                $"  2. Среднее число занятых приборов:\r\n" +
+                $"     Эмпирика: {avgBusyServers:F4}\r\n" +
+                $"     Доля занятости: {p1Emp * 100:F1}%\r\n\r\n" +
+                $"  3. Средняя длина очереди:\r\n" +
+                $"     Эмпирика: {avgQueueLength:F4}\r\n\r\n" +
+                $"  4. Вероятность отказа (стат.):\r\n" +
+                $"     P_отк = rejected/N = {p_rej_stat:F4}\r\n\r\n" +
+                $"  5. Текущее состояние в конце цикла:\r\n" +
+                $"     x = {x} (занято) | y = {y} (в очереди)\r\n" +
+                "═══════════════════════════════════════";
+
+            DrawChart(probabilities, N);
+        }
+
+        private void DrawChart(double[] probabilities, int N)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            chart1.Legends.Clear();
+
+            ChartArea area = new ChartArea();
+            area.AxisY.Title = "Вероятность";
+            area.AxisY.Minimum = 0;
+            area.AxisY.Maximum = 1.0;
+            area.AxisY.Interval = 0.1;
+            area.AxisX.Interval = 1;
+            chart1.ChartAreas.Add(area);
+
+            Series sEmp = new Series("Эмпирика");
+            sEmp.ChartType = SeriesChartType.Column;
+            sEmp.Color = Color.LightGreen;
+            sEmp.IsValueShownAsLabel = true;
+            sEmp.LabelFormat = "{0:F2}";
+
+            for (int i = 0; i < probabilities.Length; i++)
+            {
+                int pointIndex = sEmp.Points.AddXY(i, probabilities[i]);
+                if (i <= N)
+                {
+                    sEmp.Points[pointIndex].Color = Color.LightGreen;
+                }
+                else
+                {
+                    sEmp.Points[pointIndex].Color = Color.Coral;
+                }
+            }
+
+            chart1.Series.Add(sEmp);
+
+            for (int i = 0; i < probabilities.Length; i++)
+            {
+                CustomLabel label = new CustomLabel();
+                label.FromPosition = i - 0.5;
+                label.ToPosition = i + 0.5;
+                label.Text = $"P{i}";
+                area.AxisX.CustomLabels.Add(label);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class MultRandom
+    {
+        private long x_mult;
+        private const long c = (long)int.MaxValue / (2 * 10) + 3;
+        private const long m = (long)int.MaxValue;
+
+        public MultRandom(long seed)
+        {
+            x_mult = seed;
+        }
+
+        public double Next()
+        {
+            x_mult = (c * x_mult) % m;
+            return (double)x_mult / m;
+        }
+    }
+}
